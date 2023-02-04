ea5b1752ba469847c756b2da7c134dadc866d87b
Piyush <piyush.dhamecha@gmail.com>
2023-02-04 12:10:16 +0530

    Class life cycle

--- src/EventHandlers/App.js
+++ src/EventHandlers/App.js
@@ -1,15 +1,38 @@
 // import EventHandlers from "./EventHanlders"
+import { Component } from "react";
 import EventHandlersClass from "./EventHandlersClass";
 
-const App = () => {
-  return (
-    <div>
-      <EventHandlersClass />
-      <EventHandlersClass />
-      <EventHandlersClass />
-      <EventHandlersClass />
-    </div>
-  );
-};
+// const App = () => {
+
+//   return (
+//     <div>
+//       <EventHandlersClass />
+//     </div>
+//   );
+// };
+
+class App extends Component{
+  constructor() {
+    super()
+
+    this.state = {
+      showComponent: false
+    }
+  }
+
+  handleToggleComponent = () => {
+    this.setState({ showComponent: !this.state.showComponent})
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleToggleComponent}>{this.state.showComponent ? 'Hide': 'Show'}</button>
+        {this.state.showComponent && <EventHandlersClass />}
+      </div>  
+    )
+  }
+}
+
 
 export default App;
--- src/EventHandlers/EventHandlersClass.js
+++ src/EventHandlers/EventHandlersClass.js
@@ -2,15 +2,46 @@ import { Component } from "react"
 
 class EventHandlersClass extends Component {
   constructor() {
+    console.log('Constructor called')
     super()
 
     this.state = {
-      count: 0
+      count: 0,
+      country: "",
+      time: new Date(),
+      timerInstance: null
     }
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount called')
+    // const timerInstance = setInterval(() => {
+    //   this.setState({ time: new Date()})
+    // }, 2000);
+
+    // this.setState({ timerInstance })
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('ComponentDidUpdate called')
+
+    // console.log({ prevState, currentState: this.state })
+
+    // if(prevState.country !== this.state.country) {
+    //   console.log('Do some dropdown actions')
+    // }
+  }
+
+  componentWillUnmount() {
+    console.log('ComponentWillUnmount Called')
+    
+    // clearInterval(this.state.timerInstance)
   }
 
   handleIncrementClick = () => {
     this.setState({ count: this.state.count + 1 })
+    // this.state.count = this.state.count + 1
+    debugger
     console.log('Increment clicked')
   }
 
@@ -19,14 +50,23 @@ class EventHandlersClass extends Component {
     console.log('Decrement clicked')
   }
 
+  handleDropdownChange = (e) => {
+    this.setState({ country: e.target.value })
+  }
 
   render() {
-    console.log(this)
+    console.log('Render called')
     return (
       <div>
+        {/* <div>{this.state.time.toTimeString().split(' ')[0]}</div> */}
         <div>{this.state.count}</div>
         <button onClick={this.handleIncrementClick}>Increment</button>
         <button onClick={this.handleDecrementClick}>Decrement</button>
+        {/* <select onChange={this.handleDropdownChange}>
+          <option value=""></option>
+          <option value="India">India</option>
+          <option value="UK">UK</option>
+        </select> */}
       </div>
     )
   }
